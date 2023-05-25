d090f1a41c794f9e1b855b568936990bdc1ed903
Venkatesh <107678214+venky0357@users.noreply.github.com>
2023-05-25 15:47:35 +0530

    final commit

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,16 @@
-
-export default function Square({value}) {
-  return (
-   <button  type="button" className="square" >{value}</button>  
-  )
-}
+const Square = ({ value, onClick, isWinningSquare }) => {
+    const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+    const winningClassName = isWinningSquare ? 'winning' : '';
+  
+    return (
+      <button
+        type="button"
+        className={`square ${colorClassName} ${winningClassName}`}
+        onClick={onClick}
+      >
+        {value}
+      </button>
+    );
+  };
+  
+  export default Square;
